891e0997ca2413c016e94beefc792816df3577b7
Danya0103 <user@WIN-LRBB7CDB9IA>
2025-12-09 13:39:37 +0200

    Commit

--- Danya_15_11_2025/Pages/Books/Delete.cshtml.cs
+++ Danya_15_11_2025/Pages/Books/Delete.cshtml.cs
@@ -1,11 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.AspNetCore.Mvc;
+using Danya_15_11_2025.Data;
+using Danya_15_11_2025.Models;
 namespace Danya_15_11_2025.Pages.Books;
 
-public class Delete : PageModel
+public class DeleteModel : PageModel
 {
-    public void OnGet()
+    private readonly LibraryContext _context;
+
+    public DeleteModel(LibraryContext context)
     {
+        _context = context;
+    }
+
+    public Book bookItem { get; set; }
+    
+    public async Task<IActionResult> OnGetAsync(int ID)
+    {
+        bookItem = await _context.Books.FindAsync(ID);
+        if (bookItem == null)
+        {
+            return NotFound();
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int ID)
+    {
+        var book = await _context.Books.FindAsync(ID);
+        if (book != null)
+        {
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+        }
+         return RedirectToPage("Index");
         
     }
+    
+  
 }
--- Danya_15_11_2025/Pages/Books/Edit.cshtml.cs
+++ Danya_15_11_2025/Pages/Books/Edit.cshtml.cs
@@ -1,11 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.AspNetCore.Mvc;
+using Danya_15_11_2025.Data;
+using Danya_15_11_2025.Models;
 namespace Danya_15_11_2025.Pages.Books;
 
-public class Edit : PageModel
+public class EditModel : PageModel
 {
-    public void OnGet()
+    private readonly LibraryContext _context;
+
+    public EditModel(LibraryContext context)
+    {
+        _context = context;
+    }
+    
+    [BindProperty]
+    public Book bookItem { get; set; }
+    
+    public async Task<IActionResult> OnGetAsync(int ID)
+    {
+        bookItem = await _context.Books.FindAsync(ID);
+        if (bookItem == null)
+        {
+            return NotFound();
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int ID)
     {
         
+            _context.Books.Update(bookItem);
+            await _context.SaveChangesAsync();
+        
+        return RedirectToPage("Index");
+        
     }
+    
+  
 }
